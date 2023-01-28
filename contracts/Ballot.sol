746ff4f4190eb49dbaeed58e5e2650769a867cd1
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-01-28 20:48:11 +0530

    rightToVote and vote

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -27,4 +27,23 @@ contract Ballot {
             proposals.push(Proposal({name: proposalNames[i], voteCount: 0}));
         }
     }
+
+    function giveRightToVote(address voter) public {
+        require(
+            msg.sender == chairperson,
+            "Only the Chairperson can give access to vote."
+        );
+        require(!voters[voter].voted, "The voter has already voted");
+        require(voters[voter].weight == 0);
+        voters[voter].weight == 1;
+    }
+
+    function vote(uint256 proposal) public {
+        Voter storage sender = voters[msg.sender];
+        require(sender.weight != 0, "Has no right to vote");
+        require(!sender.voted, "Already voted");
+        sender.voted = true;
+        sender.vote = proposal;
+        proposals[proposal].voteCount += sender.weight;
+    }
 }

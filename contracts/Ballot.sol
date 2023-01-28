7c73cc8a40e0f05f0e681492dc7c85843c83334f
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-01-28 20:40:44 +0530

    constructor basic

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -10,10 +10,21 @@ contract Ballot {
 
     struct Proposal {
         bytes32 name;
-        uint256 vouteCount;
+        uint256 voteCount;
     }
 
     Proposal[] public proposals;
 
     mapping(address => Voter) public voters;
+
+    address public chairperson;
+
+    constructor(bytes32[] memory proposalNames) {
+        chairperson = msg.sender;
+        voters[chairperson].weight = 1;
+
+        for (uint256 i = 0; i < proposalNames.length; i++) {
+            proposals.push(Proposal({name: proposalNames[i], voteCount: 0}));
+        }
+    }
 }
